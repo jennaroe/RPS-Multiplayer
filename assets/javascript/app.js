1e370ff9956215c19f2fd41207ba5324e60321bb
Jennifer Rose <jenniferrose@jennifers-mbp-2.attlocal.net>
2016-05-18 23:06:37 -0400

    with buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,99 +4,105 @@ $( document ).ready(function() {
 var dataRef = new Firebase("https://rpsactive.firebaseio.com/");
     //setting a variable 
     var rps = "https://rpsactive.firebaseio.com/"
+    var playerNum;
     var players = 'players';
     var player1 = '1';
     var player2 = '2';
     var turns = 'turns';   
     var wins = 0;
     var losses = 0;
-    var opponentPlayerNum;
     var ticker = 0;
     
     // Onclick function
     $("#addPlayer").on("click", function(e) {
         e.preventDefault();
-    var player = $('#playerinput').val().trim();
-     console.log(player)
+        var player = $('#playerinput').val().trim();
+         console.log(player)
+            $('#playerinput').empty;
+// 
+        dataRef.once("value", function(snapshot) {
+            var exists = snapshot.child(players).exists();
+            var full = snapshot.child(players).child('2').exists();
+            assignPlayer(player, dataRef, exists, full);
 
-    $('.player1').html(player);
-    $('.player2').html(player);
-    
-    dataRef.once("value", function(snapshot) {
-    var exists = snapshot.child(players).exists();
-    var full = snapshot.child(players).child('2').exists();
-      assignPlayer(player, dataRef, exists, full);
+        });
 
-});
-    return false;
+        return false;
     });
 
 function assignPlayer(player, dataRef, exists, full){
 var playerNum;
-  dataRef.on("value", function(snapshot) {
-              $('.player2').text(snapshot.val().players[opponentPlayerNum].userId);
-              $('#scoreBox').text('Wins: ' + wins +' Losses: ' + losses);
-            $('#scores2').text('Wins: ' + snapshot.val().players[opponentPlayerNum].wins + ' Losses: ' + snapshot.val().players[opponentPlayerNum].losses);
-             });
 
   if(exists && !full){
-    var playersRef = dataRef.child(players);
-
+    var playersRef = dataRef.child(players)
     var player2Ref = playersRef.child(player2);
-
-    $('.player2').html(player2Ref.player);
+    $('player2').empty;
+    player2Ref.onDisconnect().remove();
+    renderButtons();
+    console.log(player2Ref);
 
       player2Ref.set({
         player: player, 
         wins: wins, 
         losses: losses
         });
-
-      playerNum = 2;
-      playerRef.onDisconnect().remove();
   }
   else if(full){
     alert('The game is full, try again later');
+    $('#player-form').empty();
   }
    else{ 
     var playersRef = dataRef.child(players);
     var player1Ref = playersRef.child(player1);
-
-    
+    console.log(player1Ref);
+    player1Ref.onDisconnect().remove();
+    renderButtons();
       player1Ref.set({
         player: player, 
         wins: wins, 
         losses: losses
         });
-      playerNum = 1;
 
-       $('.player1').html(player1Ref.player);
     }
 };
 
 
-    // dataRef.on("value", function(snapshot, assignPlayer) {
-
-    //       var playersRef = rps.dataRef.child(rps.players);
-
-    //       var player1Ref = playersRef.child(rps.player1);
-
-    //       var player2Ref = playersRef.child(rps.player2);
-function drawButtons(){
-           $('#rpsButtons').html('<div class="list-group"><button type="button" class="list-group-item choice" data-text="r">Rock</button><button type="button" class="list-group-item choice" data-text="p">Paper</button><button type="button" class="list-group-item choice" data-text="s">Scissors</button></div>');
-}
-        
-    //     console.log(player1Ref)},
-      function (errorObject) {
+    dataRef.on("child_added", function(snapshot) {
+        var user1 = snapshot.child('1').val();
+        var user2= snapshot.child('2').val();
+        console.log(user1)
+        console.log(user2)
+        playerLink(user1, user2)
+    }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     });
 
+    function playerLink(player1, player2) {
+        $('#player1').prepend('<p>'+ player1.player + '</p>');
+        $('#scores1').html('Wins: ' + player1.wins +' Losses: ' + player1.losses);
 
 
+        $('#player2').prepend('<p>'+ player2.player + '</p>');
+        $('#scores2').html('Wins: ' + player2.wins +' Losses: ' + player2.losses);
+    };
+
+    function renderButtons(){
+      $('.rpsButtons').empty();
+      var buttons = ['Rock', 'Paper', 'Scissors']
+      
+      for (var i = 0; i < buttons.length; i++){
+
+      var b = $('<button>')
+      b.attr('type = button')
+      b.addClass('rps list-group-item list-group-item-success');
+      b.attr('data-name', buttons[i]);
+      b.text(buttons[i]);
+      $('.rpsButtons').append(b);
+      
+      };
+    };
+    renderButtons();
 });
 
-
-      
-
-    
+  
   
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,46 @@
-
 $( document ).ready(function() {
    
 
 $('.signIn').on('click', function(){
+  
   var userId = $('.userId').val().trim();
   var gameRef = new Firebase('https://rpsactive.firebaseio.com/');
+
   gameRef.once("value", function(snapshot) {
-  var exists = snapshot.child(players).exists();
-  var full = snapshot.child(players).child('2').exists();
-  assignPlayer(userId, gameRef, exists, full);
-  $('.player').html(userId);
+                var p1exists = snapshot.child(players).child('1').exists();
+                var p2exists = snapshot.child(players).child('2').exists();
+
+                assignPlayer(userId, gameRef, p1exists, p2exists);
+
+                $('.player').html(userId);
+
+                var messegeRef = gameRef.child('messeges');
+                var timeRef = gameRef.child('timeStamp');
+
+                timeRef.onDisconnect().remove();
+                messegeRef.onDisconnect().update({messege: null});
+
+                $('.send').on('click', function(){
+                                
+                                messege = $('.messege').val().trim();
+                                messegeRef.update({messege: userId +': ' + messege})
+                                timeRef.push({time: Firebase.ServerValue.TIMESTAMP})
+                                
+
+                });
+
+                messegeRef.on('value', function(snapshot){
+                                newMessege = snapshot.val().messege;
+                                $('.chatBox').append('<p>'+ newMessege +'</p>')                 
+                });
+
+                timeRef.on('child_added', function(snapshot){
+                            timeSt = snapshot.val().time;
+                            newTime = moment(timeSt).format('LT');
+                            $('.chatBox').append('<p class="time">'+ newTime +'</p>') 
+                });
   
-});
+  });
   
 });
 
@@ -26,141 +55,150 @@ var losses = 0;
 var opponentPlayerNum;
 var ticker = 0;
 
+function getGif(status){
+  var random = Math.floor(Math.random() * 80);
+  var api = 'http://api.giphy.com/v1/gifs/search?q='+ status +'&limit=1&offset='+ random +'&api_key=dc6zaTOxFJmzC';
+         $.ajax({
+              url: api,
+              method: 'GET'
+              }).done(function(response) {
+                  console.log(response);
+                    var URL = response.data[0].images.fixed_height.url;                   
+                    var giff = $('<div>')
+                    var img = $('<img>').addClass('image').attr('src', URL);
+                    giff.append(img)
+                    $('.results').append(giff);        
+         });
+}
+
+function assignPlayer(userId, gameRef, p1exists, p2exists){
+        var playerNum;
+        gameRef.on("value", function(snapshot) {
+                    $('.player2').text(snapshot.val().players[opponentPlayerNum].userId);
+                    $('.playerResults').text('Wins: ' + wins +' Losses: ' + losses);
+                    $('.oppResults').text('Wins: ' + snapshot.val().players[opponentPlayerNum].wins + ' Losses: ' + snapshot.val().players[opponentPlayerNum].losses);
+        });
+
+        if(p1exists && !p2exists){  
+            var playersRef = gameRef.child(players);
+            var playerRef = playersRef.child(player2);
+            playerRef.set({userId: userId, wins: wins, losses: losses});
+            playerNum = 2;
+            playerRef.onDisconnect().remove();
+        }
+
+        else if(p1exists && p2exists){
+                alert('The game is full, try again later');
+        }
+
+        else{ var playersRef = gameRef.child(players);
+              var playerRef = playersRef.child(player1);
+              playerRef.set({userId: userId, wins: wins, losses: losses});
+              playerNum = 1;
+              playerRef.onDisconnect().remove();
+        }
 
 
+function playGame(playerNum, playerRef, playersRef){
+var turnsRef=gameRef.child(turns);
+turnsRef.onDisconnect().remove();
+opponentPlayerNum = playerNum === 1 ? 2 : 1;
+turnsRef.set({turn:1})
 
-function assignPlayer(userId, gameRef, exists, full){
+turnsRef.on("value", function(snapshot) {
+        var currentTurn = snapshot.val().turn;
 
-  var playerNum;
-  gameRef.on("value", function(snapshot) {
-              $('.player2').text(snapshot.val().players[opponentPlayerNum].userId);
-              $('.playerResults').text('Wins: ' + wins +' Losses: ' + losses);
-              $('.oppResults').text('Wins: ' + snapshot.val().players[opponentPlayerNum].wins + ' Losses: ' + snapshot.val().players[opponentPlayerNum].losses);
-             });
-             
-  if(exists && !full){  var playersRef = gameRef.child(players);
-                        var playerRef = playersRef.child(player2);
-                        playerRef.set({userId: userId, wins: wins, losses: losses});
-                        playerNum = 2;
-                        playerRef.onDisconnect().remove();
+        function reset(){
+                $('.playerStuff').empty();
+                $('.oppStuff').empty();
+                $('.results').empty();
+                currentTurn = 1;
+                turnsRef.remove();
+                turnsRef.off();
+                playerRef.update({choice:null});
+                playGame(playerNum, playerRef, playersRef);
+        }
 
-  }
-  else if(full){
-    alert('The game is full, try again later');
-  }
-   else{ var playersRef = gameRef.child(players);
-         var playerRef = playersRef.child(player1);
-         playerRef.set({userId: userId, wins: wins, losses: losses});
-         playerNum = 1;
-         playerRef.onDisconnect().remove();
-    }
+        function timeUp(){
+                  clearInterval(tick);
+                  ticker = 0;
+        }
 
+        function timer(){
+                tick = setInterval(count, 1000);
+        }
 
-    function playGame(playerNum, playerRef, playersRef){
-            var turnsRef=gameRef.child(turns);
-            opponentPlayerNum = playerNum === 1 ? 2 : 1;
-            var messegeRef = gameRef.child('messeges');
-            messegeRef.onDisconnect().update({messege: null});
-            $('.send').on('click', function(){
-              messege = $('.messege').val().trim();
-              messegeRef.update({messege: userId +': ' + messege})
+        function count(){
+                ticker++;
+                if(ticker == 5) {
+                                  timeUp();
+                                  reset();
+                }
+        } 
+
+        var choice;
+
+        if(playerNum == snapshot.val().turn){
+            $('.info').empty();
+            drawButtons();
+
+            $('body').on('click', '.choice', function(){
+                          choice = $(this).data('text');
+                          playerRef.update({choice: choice});
+                          currentTurn++;
+                          turnsRef.set({turn:currentTurn});
+                          $('.playerStuff').html('<h1>'+ choice +'</h1>')
             })
-            messegeRef.on('value', function(snapshot){
-              newMessege = snapshot.val().messege;
-              $('.chatBox').append('<p>'+ newMessege +'</p>') 
-            });
+        }
 
-            turnsRef.set({turn:1})
+        else if(snapshot.val().turn == 3){
+                $('.info').empty();
 
-            turnsRef.on("value", function(snapshot) {
-              console.log(snapshot.val().turn);
+                gameRef.once("value", function(snapshot) {                                                  
+                              var myChoice = snapshot.val().players[playerNum].choice;
+                              var oppChoice = snapshot.val().players[opponentPlayerNum].choice;                                                  
+                              var result = snapshot.val().players[playerNum];
+                              
+                              if(myChoice == oppChoice){
+                                  $('.results').html('<h1>DRAW</h1>');
+                                  $('.oppStuff').html('<h1>'+ oppChoice +'</h1>')
 
-              var currentTurn = snapshot.val().turn;
-              function reset(){
-                        $('.playerStuff').empty();
-                        $('.oppStuff').empty();
-                        $('.results').empty();
-                        currentTurn = 1;
-
-                        turnsRef.remove();
-                        turnsRef.off();
-                        playerRef.update({choice:null});
-                        playGame(playerNum, playerRef, playersRef);
-                      }
-                      function timeUp(){
-                            clearInterval(tick);
-                            ticker = 0;
+                                    timer();
                               }
-                      function timer(){
-                          tick = setInterval(count, 1000);
-                      }
-                      function count(){
-                              ticker++;
-                              if(ticker == 3) {
-                                timeUp();
-                                reset();
+
+                              else if(myChoice == "Rock" && oppChoice == "Scissors" || myChoice == "Paper" && oppChoice == "Rock" || myChoice =="Scissors" && oppChoice == "Paper"){
+                                      wins++;
+                                      playerRef.update({wins: wins});
+                                      $('.info').html('<h1>YOU WON</h1>');
+                                      $('.oppStuff').html('<h1>'+ oppChoice +'</h1>')
+                                      getGif('winner');
+                                      timer();
                               }
-                      }      
-                    var choice;
-                    if(playerNum == snapshot.val().turn){
-                      $('.info').empty();
-                      drawButtons();
-                      $('body').on('click', '.choice', function(){
-                        choice = $(this).data('text');
-                        playerRef.update({choice: choice});
-                        currentTurn++;
-                        turnsRef.set({turn:currentTurn});
-                        $('.playerStuff').html('<h1>'+ choice +'</h1>')
-                      })
-                    }
-                    else if(snapshot.val().turn == 3){
-                      $('.info').empty();
-                      gameRef.once("value", function(snapshot) {
-                        console.log(snapshot.val());
-                        var myChoice = snapshot.val().players[playerNum].choice;
-                        var oppChoice = snapshot.val().players[opponentPlayerNum].choice;
-                        console.log(oppChoice);
-                        var result = snapshot.val().players[playerNum];
-                        console.log(myChoice);
-                        if(myChoice == oppChoice){
-                            $('.results').html('<h1>DRAW</h1>');
-                            $('.oppStuff').html('<h1>'+ oppChoice +'</h1>')
-                            timer();
-                        }
-                        else if(myChoice == "r" && oppChoice == "s" || myChoice == "p" && oppChoice == "r" || myChoice =="s" && oppChoice == "p"){
-                          wins++;
-                          playerRef.update({wins: wins});
-                          $('.results').html('<h1>YOU WON</h1>');
-                          $('.oppStuff').html('<h1>'+ oppChoice +'</h1>')
-                          timer();
-                        }
-                        else if(myChoice == "r" && oppChoice == "p" || myChoice == "p" && oppChoice == "s" || myChoice =="s" && oppChoice == "r"){
-                            losses++;
-                            playerRef.update({losses: losses});
-                            $('.results').html('<h1>'+ snapshot.val().players[opponentPlayerNum].userId +' Wins</h1>');
-                            $('.oppStuff').html('<h1>'+ oppChoice +'</h1>')
-                            timer();
-                        }
-                        });
-                    }
-                    else{
-                      $('.info').html('Waiting for opponenet');
-                      }
-                      
-            });
-    }
+
+                              else if(myChoice == "Rock" && oppChoice == "Paper" || myChoice == "Paper" && oppChoice == "Scissors" || myChoice =="Scissors" && oppChoice == "Rock"){
+                                      losses++;
+                                      playerRef.update({losses: losses});
+                                      $('.info').html('<h1>'+ snapshot.val().players[opponentPlayerNum].userId +' Wins</h1>');
+                                      $('.oppStuff').html('<h1>'+ oppChoice +'</h1>')
+                                      getGif('loser');
+                                      timer();
+                              }
+                  });
+      }
+
+      else{
+          $('.info').html('Waiting for opponenet');
+      }
+
+  });
+        }
 
   playGame(playerNum, playerRef, playersRef);
+
 }
 
 function drawButtons(){
-           $('.playerStuff').html('<div class="list-group"><button type="button" class="list-group-item choice" data-text="r">Rock</button><button type="button" class="list-group-item choice" data-text="p">Paper</button><button type="button" class="list-group-item choice" data-text="s">Scissors</button></div>');
+        $('.playerStuff').html('<div class="list-group rps"><button type="button" class="list-group-item choice" data-text="Rock">Rock</button><button type="button" class="list-group-item choice" data-text="Paper">Paper</button><button type="button" class="list-group-item choice" data-text="Scissors">Scissors</button></div>');
 }
 
 });
-
-
-      
-
-    
-  
